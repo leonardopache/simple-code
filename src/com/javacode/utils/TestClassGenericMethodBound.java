d61d82dab279c23cdca4148ad005971a1bea218b
leonardopache <leonardo@pache.eng.br>
2017-10-22 13:06:19 +0200

    include generic method implementation

--- src/com/javacode/utils/TestClassGenericMethodBound.java
+++ src/com/javacode/utils/TestClassGenericMethodBound.java
@@ -1,34 +1,59 @@
+package com.javacode.utils;
 
-package com.javacod.utils;
+/**
+ * Simple code how to use generic methods.
+ *
+ * @author lpache
+ */
+import java.util.logging.Logger;
 
-public class TestClassGenericMethodBound extends Generic{
+public class TestClassGenericMethodBound extends Generic {
+	private static final Logger LOG = Logger.getLogger(TestClassGenericMethodBound.class.getName());
 
 	public static void main(String[] args) {
-		TestClassGenericMethodBound t = new TestClassGenericMethodBound();
-		validId(t);
-		
+		TestClassGenericMethodBound testClass = new TestClassGenericMethodBound();
+		validId(testClass);
 	}
-	public static <U extends IDGeneric> U validId(U obj){
-		obj.getId();
+
+	public static <U extends IDGeneric> U validId(U obj) {
+		try {
+			LOG.info("ID from super class: " + obj.getClass().getSuperclass().getMethod("getId", (Class<?>[]) null));
+		} catch (NoSuchMethodException | SecurityException e) {
+			LOG.warning(e.getMessage());
+		}
 		return obj;
 	}
-
 }
 
-class IDGeneric{
-	public String getId(){
-		return "ID";
-	}
-}
+class IDGeneric {
+	private Long id;
 
-class Generic extends IDGeneric{
-	private String id;
-
-	public String getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(String id) {
+	public void setId(Long id) {
 		this.id = id;
+	}
+}
+
+class Generic extends IDGeneric {
+	private String name;
+	private String mail;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
 	}
 }
