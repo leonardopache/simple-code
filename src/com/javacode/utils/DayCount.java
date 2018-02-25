14d22a69994aff922dc83f99d7e4ace9bd192a24
Leonardo <leonardo@pache.eng.br>
2018-02-25 16:48:04 +0100

    fix calendar implementation

--- src/com/javacode/utils/DayCount.java
+++ src/com/javacode/utils/DayCount.java
@@ -1,6 +1,5 @@
 package com.javacode.utils;
 
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -26,25 +25,61 @@ public class DayCount {
 	}
 
 	public static int countDays(Calendar beginning, Calendar ending) {
-		return ending.get(Calendar.DAY_OF_YEAR) - beginning.get(Calendar.DAY_OF_YEAR);
+		int days = 0;
+		while(beginning.before(ending) && beginning.getTimeInMillis() != ending.getTimeInMillis()) {
+			days++;
+			beginning.add(Calendar.DAY_OF_YEAR, 1);
+		}
+		return days;
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
+		//JodaTime
 		int leoDays = newCountDays(newCalendar(LEO_START).getTimeInMillis(), Calendar.getInstance().getTimeInMillis());
 		int juDays = newCountDays(newCalendar(JU_START).getTimeInMillis(), Calendar.getInstance().getTimeInMillis());
 		LOG.info("Leo days: "+ leoDays);
 		LOG.info("Jú days: "+ juDays);
+		
+		//Calendar
+		int leoDays1 = countDays(newCalendar(LEO_START), newCalendarInstance(true));
+		int juDays1 = countDays(newCalendar(JU_START), newCalendarInstance(true));
+		LOG.info("Leo days: "+ leoDays1);
+		LOG.info("Jú days: "+ juDays1);
 	}
 	
+	private static Calendar newCalendarInstance(boolean resetTime) {
+		Calendar date = Calendar.getInstance();
+		if(resetTime) {
+			return resetTimeFromDate(date);
+		}
+		return date;
+	}
+
+	public static final Calendar resetTimeFromDate(Calendar calendar) {
+
+		if (calendar == null)
+			return null;
+
+		Calendar reseted = Calendar.getInstance();
+		reseted.clear();
+		reseted.set(Calendar.YEAR, calendar.get(Calendar.YEAR));
+		reseted.set(Calendar.MONTH, calendar.get(Calendar.MONTH));
+		reseted.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH));
+
+		reseted.getTime(); // força atualização do timeInMills
+
+		return reseted;
+	}
+
 	private static int newCountDays(long startTime, long timeNow) {
 		Days days = Days.daysBetween(new DateTime(startTime), new DateTime(timeNow));
 		return days.getDays();
 	}
 
 	private static Calendar newCalendar(String date) {
-		Calendar begin = Calendar.getInstance();
-		begin.setTimeInMillis(dateTimeFormatter(date, DMY_FORMATTER));
-		return begin;
+		Calendar dateTime = Calendar.getInstance();
+		dateTime.setTimeInMillis(dateTimeFormatter(date, DMY_FORMATTER));
+		return dateTime;
 	}
 
 	private static Long dateTimeFormatter(String date, String pattern) {
