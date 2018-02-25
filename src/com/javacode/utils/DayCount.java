c03049c0cafc239dee9f9130b0faacc1d287241c
Leonardo <leonardo@pache.eng.br>
2018-02-25 16:07:36 +0100

    update to jodaTime

--- src/com/javacode/utils/DayCount.java
+++ src/com/javacode/utils/DayCount.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.logging.Logger;
+
+import org.joda.time.DateTime;
+import org.joda.time.Days;
 
 /**
  * 
@@ -12,7 +16,10 @@ import java.util.Calendar;
  */
 public class DayCount {
 
+	private static final String JU_START = "15/05/2017";
+	private static final String LEO_START = "01/06/2017";
 	private static final String DMY_FORMATTER = "dd/MM/yyyy";
+	private static final Logger LOG = Logger.getLogger(DayCount.class.getName());
 
 	public DayCount() {
 		throw new InstantiationError("Utility class, do not instantiate!!!");
@@ -23,10 +30,17 @@ public class DayCount {
 	}
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Leo days: " + countDays(newCalendar("01/06/2017"), Calendar.getInstance()));
-		System.out.println("Jú days: " + countDays(newCalendar("15/05/2017"), Calendar.getInstance()));
+		int leoDays = newCountDays(newCalendar(LEO_START).getTimeInMillis(), Calendar.getInstance().getTimeInMillis());
+		int juDays = newCountDays(newCalendar(JU_START).getTimeInMillis(), Calendar.getInstance().getTimeInMillis());
+		LOG.info("Leo days: "+ leoDays);
+		LOG.info("Jú days: "+ juDays);
 	}
 	
+	private static int newCountDays(long startTime, long timeNow) {
+		Days days = Days.daysBetween(new DateTime(startTime), new DateTime(timeNow));
+		return days.getDays();
+	}
+
 	private static Calendar newCalendar(String date) {
 		Calendar begin = Calendar.getInstance();
 		begin.setTimeInMillis(dateTimeFormatter(date, DMY_FORMATTER));
